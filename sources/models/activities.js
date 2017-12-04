5ee59598241a59d010950a87bfbeea8a15aac36b
DmitriySlabodchikov <32464960+DmitriySlabodchikov@users.noreply.github.com>
2017-12-04 13:29:06 +0300

    date

--- sources/models/activities.js
+++ sources/models/activities.js
@@ -1,6 +1,18 @@
 export const activities = new webix.DataCollection({
 	url: "http://localhost:8096/api/v1/activities/",
-	save: "rest->http://localhost:8096/api/v1/activities/"
+	save: "rest->http://localhost:8096/api/v1/activities/",
+	scheme: {
+		$init: function (obj) {
+			if (obj.DueDate) {
+				obj.DueDate = webix.i18n.parseFormatDate(obj.DueDate);
+			}
+		},
+		$save: function (obj) {
+			if (obj.DueDate) {
+				obj.DueDate = webix.i18n.parseFormatStr(obj.DueDate);
+			}
+		}
+	}
 });
 
 export function getActivity(id) {
